33454ef0b2566ba0feefed11b8f9bbb26b3c3bf3
hussamshannan <info@shark-plan.com>
2025-07-09 22:29:30 +0300

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -50,9 +50,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.get("/", () => {
-  res.send("hi hussam");
-});
+
 // Register Routes
 app.use("/category", categoryRouter);
 app.use("/email", emailRouter);
